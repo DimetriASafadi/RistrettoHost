9b0f5fbe8ce2eea73796f7259dbd10363ae72b36
Dimetri A Safadi <IbrahimASafadi99@hotmail.com>
2022-08-15 16:01:15 +0300

    gg

--- app/src/main/java/com/dimetris/ristrettohost/CommonsSection/Constants.kt
+++ app/src/main/java/com/dimetris/ristrettohost/CommonsSection/Constants.kt
@@ -1,8 +1,11 @@
 package com.dimetris.ristrettohost.CommonsSection
 
+import com.dimetris.ristrettohost.Models.RISCartItem
+
 object Constants {
 
     const val AppSPName = "RistrettorHostPreferences"
     const val KeyAppLanguage = "AppLanguage"
+    val cartItems = ArrayList<RISCartItem>()
 
 }
--- app/src/main/java/com/dimetris/ristrettohost/RecViews/PricesRecView.kt
+++ app/src/main/java/com/dimetris/ristrettohost/RecViews/PricesRecView.kt
@@ -1,9 +1,11 @@
 package com.dimetris.ristrettohost.RecViews
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dimetris.ristrettohost.Models.RISCost
@@ -11,6 +13,8 @@ import com.dimetris.ristrettohost.R
 
 class PricesRecView(val data : ArrayList<RISCost>, val context: Context) : RecyclerView.Adapter<PriceViewHolder>() {
 
+
+    var selectedItem = 0
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PriceViewHolder {
         return PriceViewHolder(LayoutInflater.from(context).inflate(R.layout.rec_item_price, parent, false))    }
@@ -24,17 +28,26 @@ class PricesRecView(val data : ArrayList<RISCost>, val context: Context) : Recyc
 
     override fun onBindViewHolder(holder: PriceViewHolder, position: Int) {
 
-
+        if (selectedItem == position){
+            holder.PriceValue.backgroundTintList = ColorStateList.valueOf(context.getColor(R.color.ris_light_green))
+        }else{
+            holder.PriceValue.backgroundTintList = null
+        }
 
         holder.PriceName.text = data[position].CostName
         holder.PriceValue.text = data[position].CostValue.toString() + " شيكل"
 
+        holder.WholePrice.setOnClickListener {
+            selectedItem = position
+            notifyDataSetChanged()
+        }
 
 
     }
 }
 class PriceViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     // Holds the TextView that will add each animal to
+    val WholePrice = view.findViewById<LinearLayout>(R.id.WholePrice)
     val PriceName = view.findViewById<TextView>(R.id.PriceName)
     val PriceValue = view.findViewById<TextView>(R.id.PriceValue)
 
